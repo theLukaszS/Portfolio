d3d6b3a85f62c31af14ee699c51dd4162aa928af
Łukasz S <thelukfx@gmail.com>
2023-10-01 10:49:50 +0200

    done ?

---
+++ app.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let typed = new Typed('.animate', {
+      strings: [
+        "Łukasz Słaby",
+      ],
+      typeSpeed: 100,
+      backSpeed: 80,
+      loop: true
+    });
+  });
+  
